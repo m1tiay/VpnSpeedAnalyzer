56e01c3f558d65edc2a2f9d68d04d57379feaeac
daver <58255211+m1tiay@users.noreply.github.com>
2026-04-17 00:38:40 +0300

    Update MainViewModel.cs

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -3,6 +3,8 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.IO;
 using System.Windows;
+using VpnSpeedAnalyzer.Logic;
+using VpnSpeedAnalyzer.Models;
 
 namespace VpnSpeedAnalyzer
 {
@@ -12,20 +14,34 @@ namespace VpnSpeedAnalyzer
         public event EventHandler<SpeedtestResult>? NewResultArrived;
 
         private readonly MonitorController _monitor;
+        private readonly ResultsManager _resultsManager;
 
-        public ObservableCollection<SpeedtestResult> Results { get; } = new();
+        public ObservableCollection<ResultEntry> Results { get; } = new();
 
         public MainViewModel()
         {
             _monitor = new MonitorController(this);
             _monitor.NewResult += Monitor_NewResult;
+
+            _resultsManager = new ResultsManager(Results);
         }
 
         private void Monitor_NewResult(object? sender, SpeedtestResult r)
         {
             Application.Current.Dispatcher.Invoke(() =>
             {
-                Results.Add(r);
+                Results.Add(new ResultEntry
+                {
+                    Ip = r.Ip,
+                    Country = r.Country,
+                    Timestamp = r.Timestamp.ToString("O"),
+                    Ping = r.Ping,
+                    Jitter = r.Jitter,
+                    Loss = r.Loss,
+                    Download = r.Download,
+                    Upload = r.Upload
+                });
+
                 NewResultArrived?.Invoke(this, r);
             });
         }
@@ -33,7 +49,7 @@ namespace VpnSpeedAnalyzer
         public void Start() => _monitor.Start();
         public void Stop() => _monitor.Stop();
 
-        public void ToggleBestOnly() { }
+        public void ToggleBestOnly() => _resultsManager.ApplyBestOnly();
 
         public void ExportCsv()
         {
@@ -42,10 +58,12 @@ namespace VpnSpeedAnalyzer
                 string path = $"results_{DateTime.Now:yyyyMMdd_HHmmss}.csv";
 
                 using var sw = new StreamWriter(path);
-                sw.WriteLine("Timestamp,Ping,Jitter,Loss,Download,Upload");
+                sw.WriteLine("Timestamp,IP,Country,Ping,Jitter,Loss,Download,Upload");
 
                 foreach (var r in Results)
-                    sw.WriteLine($"{r.Timestamp:O},{r.Ping},{r.Jitter},{r.Loss},{r.Download},{r.Upload}");
+                {
+                    sw.WriteLine($"{r.Timestamp},{r.Ip},{r.Country},{r.Ping},{r.Jitter},{r.Loss},{r.Download},{r.Upload}");
+                }
 
                 MessageBox.Show($"CSV saved: {path}");
             }
