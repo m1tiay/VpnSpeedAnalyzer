3ed10093b3f4992bbedb8d16c566fb3e320d963c
daver <58255211+m1tiay@users.noreply.github.com>
2026-04-17 00:07:39 +0300

    Update ResultEntry.cs

--- Models/ResultEntry.cs
+++ Models/ResultEntry.cs
@@ -2,19 +2,13 @@ namespace VpnSpeedAnalyzer
 {
     public class ResultEntry
     {
-        public string Ip { get; set; }
-        public string Country { get; set; }
+        public string Ip { get; set; } = "";
+        public string Country { get; set; } = "";
+        public string Timestamp { get; set; } = "";
         public double Ping { get; set; }
         public double Jitter { get; set; }
         public double Loss { get; set; }
         public double Download { get; set; }
         public double Upload { get; set; }
-        public string Timestamp { get; set; }
-
-        public double Score =>
-            Jitter * 10 +
-            Loss * 100 +
-            Ping * 0.5 -
-            Download * 0.01;
     }
 }
