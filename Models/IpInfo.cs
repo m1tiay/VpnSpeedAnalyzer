7beb2c0ad6d567b83477e29f619adfefbe12651d
daver <58255211+m1tiay@users.noreply.github.com>
2026-04-16 21:04:15 +0300

    Create IpInfo.cs

---
+++ Models/IpInfo.cs
@@ -0,0 +1,11 @@
+namespace VpnSpeedAnalyzer
+{
+    public class IpInfo
+    {
+        public string Ip { get; set; }
+        public string CountryCode { get; set; }
+        public string CountryName { get; set; }
+        public string Asn { get; set; }
+        public string Provider { get; set; }
+    }
+}
