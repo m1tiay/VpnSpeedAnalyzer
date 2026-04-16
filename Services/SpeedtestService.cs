495f17f2673f1afef93a92b774f7a8565897f035
daver <58255211+m1tiay@users.noreply.github.com>
2026-04-17 02:03:30 +0300

    Update SpeedtestService.cs

--- Services/SpeedtestService.cs
+++ Services/SpeedtestService.cs
@@ -4,6 +4,7 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using VpnSpeedAnalyzer.Models;
+using VpnSpeedAnalyzer.Logic;
 
 namespace VpnSpeedAnalyzer.Services
 {
