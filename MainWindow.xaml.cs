cfb8af79f21d7a7e8f9fe93d02c211a6d7c2862b
daver <58255211+m1tiay@users.noreply.github.com>
2026-04-16 23:38:21 +0300

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,7 +1,9 @@
+using ScottPlot;
+using ScottPlot.Plottable;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
-using ScottPlot.Plottable;
 
 namespace VpnSpeedAnalyzer
 {
@@ -12,8 +14,8 @@ namespace VpnSpeedAnalyzer
         private readonly List<double> _jitterData = new();
         private readonly List<double> _pingData = new();
 
-        private SignalPlot _jitterLine;
-        private SignalPlot _pingLine;
+        private ScatterPlot _jitterPlot;
+        private ScatterPlot _pingPlot;
 
         public MainWindow()
         {
@@ -22,41 +24,79 @@ namespace VpnSpeedAnalyzer
             _vm = new MainViewModel();
             DataContext = _vm;
 
-            InitPlots();
-
             _vm.NewResultArrived += Vm_NewResultArrived;
+
+            InitPlots();
         }
 
         private void InitPlots()
         {
-            _jitterLine = JitterPlot.Plot.AddSignal(_jitterData.ToArray());
-            JitterPlot.Plot.Title("Jitter (ms)");
-            JitterPlot.Plot.YLabel("ms");
-            JitterPlot.Plot.XLabel("Test #");
-            JitterPlot.Plot.SetAxisLimits(yMin: 0);
-            JitterPlot.Refresh();
+            // Jitter plot
+            _jitterPlot = WpfPlotJitter.Plot.AddScatter(
+                xs: Array.Empty<double>(),
+                ys: Array.Empty<double>(),
+                color: System.Drawing.Color.DeepSkyBlue,
+                lineWidth: 2);
 
-            _pingLine = PingPlot.Plot.AddSignal(_pingData.ToArray());
-            PingPlot.Plot.Title("Ping (ms)");
-            PingPlot.Plot.YLabel("ms");
-            PingPlot.Plot.XLabel("Test #");
-            PingPlot.Plot.SetAxisLimits(yMin: 0);
-            PingPlot.Refresh();
+            WpfPlotJitter.Plot.Title("Jitter (ms)");
+            WpfPlotJitter.Plot.XLabel("Test #");
+            WpfPlotJitter.Plot.YLabel("ms");
+            WpfPlotJitter.Refresh();
+
+            // Ping plot
+            _pingPlot = WpfPlotPing.Plot.AddScatter(
+                xs: Array.Empty<double>(),
+                ys: Array.Empty<double>(),
+                color: System.Drawing.Color.OrangeRed,
+                lineWidth: 2);
+
+            WpfPlotPing.Plot.Title("Ping (ms)");
+            WpfPlotPing.Plot.XLabel("Test #");
+            WpfPlotPing.Plot.YLabel("ms");
+            WpfPlotPing.Refresh();
         }
 
         private void Vm_NewResultArrived(object sender, SpeedtestResult r)
         {
             Dispatcher.Invoke(() =>
             {
+                // Add new data
                 _jitterData.Add(r.Jitter);
                 _pingData.Add(r.Ping);
 
-                _jitterLine.Update(_jitterData.ToArray());
-                _pingLine.Update(_pingData.ToArray());
+                // X axis = test index
+                double[] xs = Enumerable.Range(0, _jitterData.Count).Select(i => (double)i).ToArray();
 
-                JitterPlot.Refresh();
-                PingPlot.Refresh();
+                // Update jitter plot
+                _jitterPlot.Update(xs, _jitterData.ToArray());
+                WpfPlotJitter.Plot.AxisAuto();
+                WpfPlotJitter.Refresh();
+
+                // Update ping plot
+                _pingPlot.Update(xs, _pingData.ToArray());
+                WpfPlotPing.Plot.AxisAuto();
+                WpfPlotPing.Refresh();
             });
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            _vm.Start();
+        }
+
+        private void Stop_Click(object sender, RoutedEventArgs e)
+        {
+            _vm.Stop();
+        }
+
+        private void ExportCsv_Click(object sender, RoutedEventArgs e)
+        {
+            _vm.ExportCsv();
+        }
+
+        private void ShowBestOnly_Click(object sender, RoutedEventArgs e)
+        {
+            _vm.ToggleBestOnly();
         }
     }
 }
