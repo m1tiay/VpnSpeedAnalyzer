9b41373da1147d152023336d48bf40fff22fecaf
daver <58255211+m1tiay@users.noreply.github.com>
2026-04-17 02:10:54 +0300

    Update App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,8 +1,23 @@
+using System;
 using System.Windows;
+using VpnSpeedAnalyzer.Logic;
 
 namespace VpnSpeedAnalyzer
 {
     public partial class App : Application
     {
+        public App()
+        {
+            AppDomain.CurrentDomain.UnhandledException += (s, e) =>
+            {
+                Logger.Write("FATAL ERROR: " + e.ExceptionObject.ToString());
+            };
+
+            DispatcherUnhandledException += (s, e) =>
+            {
+                Logger.Write("UI ERROR: " + e.Exception.Message);
+                e.Handled = true;
+            };
+        }
     }
 }
