a7d524658aaef433a7b4a53a8a2094f2830cc2ad
daver <58255211+m1tiay@users.noreply.github.com>
2026-04-17 02:13:55 +0300

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
+using VpnSpeedAnalyzer.Logic;
 using VpnSpeedAnalyzer.Models;
 
 namespace VpnSpeedAnalyzer
@@ -19,65 +20,116 @@ namespace VpnSpeedAnalyzer
 
         public MainWindow()
         {
-            InitializeComponent();
+            try
+            {
+                Logger.Write("MainWindow ctor START");
 
-            _vm = new MainViewModel();
-            DataContext = _vm;
+                InitializeComponent();
+                Logger.Write("InitializeComponent OK");
 
-            _vm.NewResultArrived += Vm_NewResultArrived;
+                _vm = new MainViewModel();
+                Logger.Write("MainViewModel OK");
 
-            InitPlots();
+                DataContext = _vm;
+                Logger.Write("DataContext set");
+
+                _vm.NewResultArrived += Vm_NewResultArrived;
+                Logger.Write("Event subscribed");
+
+                InitPlots();
+                Logger.Write("InitPlots OK");
+            }
+            catch (Exception ex)
+            {
+                Logger.Write("MainWindow ERROR: " + ex.ToString());
+                throw;
+            }
         }
 
         private void InitPlots()
         {
-            _jitterPlot = JitterPlot.Plot.AddScatter(
-                xs: Array.Empty<double>(),
-                ys: Array.Empty<double>(),
-                color: System.Drawing.Color.DeepSkyBlue,
-                lineWidth: 2);
+            try
+            {
+                _jitterPlot = JitterPlot.Plot.AddScatter(
+                    xs: Array.Empty<double>(),
+                    ys: Array.Empty<double>(),
+                    color: System.Drawing.Color.DeepSkyBlue,
+                    lineWidth: 2);
 
-            JitterPlot.Plot.Title("Jitter (ms)");
-            JitterPlot.Plot.XLabel("Test #");
-            JitterPlot.Plot.YLabel("ms");
-            JitterPlot.Refresh();
+                JitterPlot.Plot.Title("Jitter (ms)");
+                JitterPlot.Plot.XLabel("Test #");
+                JitterPlot.Plot.YLabel("ms");
+                JitterPlot.Refresh();
 
-            _pingPlot = PingPlot.Plot.AddScatter(
-                xs: Array.Empty<double>(),
-                ys: Array.Empty<double>(),
-                color: System.Drawing.Color.OrangeRed,
-                lineWidth: 2);
+                _pingPlot = PingPlot.Plot.AddScatter(
+                    xs: Array.Empty<double>(),
+                    ys: Array.Empty<double>(),
+                    color: System.Drawing.Color.OrangeRed,
+                    lineWidth: 2);
 
-            PingPlot.Plot.Title("Ping (ms)");
-            PingPlot.Plot.XLabel("Test #");
-            PingPlot.Plot.YLabel("ms");
-            PingPlot.Refresh();
+                PingPlot.Plot.Title("Ping (ms)");
+                PingPlot.Plot.XLabel("Test #");
+                PingPlot.Plot.YLabel("ms");
+                PingPlot.Refresh();
+            }
+            catch (Exception ex)
+            {
+                Logger.Write("InitPlots ERROR: " + ex.ToString());
+                throw;
+            }
         }
 
         private void Vm_NewResultArrived(object? sender, SpeedtestResult r)
         {
-            Dispatcher.Invoke(() =>
+            try
             {
-                _jitterData.Add(r.Jitter);
-                _pingData.Add(r.Ping);
+                Dispatcher.Invoke(() =>
+                {
+                    _jitterData.Add(r.Jitter);
+                    _pingData.Add(r.Ping);
 
-                double[] xs = Enumerable.Range(0, _jitterData.Count)
-                                        .Select(i => (double)i)
-                                        .ToArray();
+                    double[] xs = Enumerable.Range(0, _jitterData.Count)
+                                            .Select(i => (double)i)
+                                            .ToArray();
 
-                _jitterPlot?.Update(xs, _jitterData.ToArray());
-                JitterPlot.Plot.AxisAuto();
-                JitterPlot.Refresh();
+                    _jitterPlot?.Update(xs, _jitterData.ToArray());
+                    JitterPlot.Plot.AxisAuto();
+                    JitterPlot.Refresh();
 
-                _pingPlot?.Update(xs, _pingData.ToArray());
-                PingPlot.Plot.AxisAuto();
-                PingPlot.Refresh();
-            });
+                    _pingPlot?.Update(xs, _pingData.ToArray());
+                    PingPlot.Plot.AxisAuto();
+                    PingPlot.Refresh();
+                });
+            }
+            catch (Exception ex)
+            {
+                Logger.Write("Vm_NewResultArrived ERROR: " + ex.ToString());
+                throw;
+            }
         }
 
-        private void Start_Click(object sender, RoutedEventArgs e) => _vm.Start();
-        private void Stop_Click(object sender, RoutedEventArgs e) => _vm.Stop();
-        private void ExportCsv_Click(object sender, RoutedEventArgs e) => _vm.ExportCsv();
-        private void ShowBestOnly_Click(object sender, RoutedEventArgs e) => _vm.ToggleBestOnly();
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            Logger.Write("Start_Click");
+            _vm.Start();
+        }
+
+        private void Stop_Click(object sender, RoutedEventArgs e)
+        {
+            Logger.Write("Stop_Click");
+            _vm.Stop();
+        }
+
+        private void ExportCsv_Click(object sender, RoutedEventArgs e)
+        {
+            Logger.Write("ExportCsv_Click");
+            _vm.ExportCsv();
+        }
+
+        private void ShowBestOnly_Click(object sender, RoutedEventArgs e)
+        {
+            Logger.Write("ShowBestOnly_Click");
+            _vm.ToggleBestOnly();
+        }
     }
 }
