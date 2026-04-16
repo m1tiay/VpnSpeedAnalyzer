d5330b314ef09d8facf97f3448ca41be6a6c66ea
daver <58255211+m1tiay@users.noreply.github.com>
2026-04-17 01:04:04 +0300

    Update SpeedtestResult.cs

--- Models/SpeedtestResult.cs
+++ Models/SpeedtestResult.cs
@@ -1,14 +1,16 @@
-using System;
-
-namespace VpnSpeedAnalyzer
+namespace VpnSpeedAnalyzer.Models
 {
     public class SpeedtestResult
     {
+        public string Ip { get; set; } = "";
+        public string Country { get; set; } = "";
+
+        public DateTime Timestamp { get; set; }
+
         public double Ping { get; set; }
         public double Jitter { get; set; }
         public double Loss { get; set; }
         public double Download { get; set; }
         public double Upload { get; set; }
-        public DateTime Timestamp { get; set; }
     }
 }
