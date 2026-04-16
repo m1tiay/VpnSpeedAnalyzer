4b6547e0e94df53f0d0f1fbac1c030a8fd3f7b7e
daver <58255211+m1tiay@users.noreply.github.com>
2026-04-17 00:50:30 +0300

    Update IpInfoService.cs

--- Services/IpInfoService.cs
+++ Services/IpInfoService.cs
@@ -1,38 +1,21 @@
 using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
+using VpnSpeedAnalyzer.Models;
 
-namespace VpnSpeedAnalyzer
+namespace VpnSpeedAnalyzer.Services
 {
     public class IpInfoService
     {
-        private static readonly HttpClient _http = new();
+        private readonly HttpClient _http = new();
 
-        public async Task<IpInfo> GetCurrentAsync()
+        public async Task<IpInfo?> GetCurrentAsync()
         {
             try
             {
-                var json = await _http.GetStringAsync("https://ipwho.is/");
-                using var doc = JsonDocument.Parse(json);
-                var root = doc.RootElement;
-
-                // ipwho.is returns { "success": false } on error
-                if (root.TryGetProperty("success", out var successProp) &&
-                    successProp.ValueKind == JsonValueKind.False)
-                {
-                    return null;
-                }
-
-                var conn = root.GetProperty("connection");
-
-                return new IpInfo
-                {
-                    Ip = root.GetProperty("ip").GetString(),
-                    CountryCode = root.GetProperty("country_code").GetString(),
-                    CountryName = root.GetProperty("country").GetString(),
-                    Asn = conn.GetProperty("asn").ToString(),
-                    Provider = conn.GetProperty("isp").GetString()
-                };
+                var json = await _http.GetStringAsync("https://ipapi.co/json/");
+                var info = JsonSerializer.Deserialize<IpInfo>(json);
+                return info;
             }
             catch
             {
