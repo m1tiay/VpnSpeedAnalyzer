d17118d2b3919883e2d437b322e0bf8b8cf08ea8
daver <58255211+m1tiay@users.noreply.github.com>
2026-04-17 02:03:52 +0300

    Update IpInfoService.cs

--- Services/IpInfoService.cs
+++ Services/IpInfoService.cs
@@ -2,6 +2,7 @@ using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
 using VpnSpeedAnalyzer.Models;
+using VpnSpeedAnalyzer.Logic;
 
 namespace VpnSpeedAnalyzer.Services
 {
