9f4929cb290d71921e2be73ce78487e9ecd6005e
daver <58255211+m1tiay@users.noreply.github.com>
2026-04-17 01:05:20 +0300

    Update IpInfo.cs

--- Models/IpInfo.cs
+++ Models/IpInfo.cs
@@ -1,11 +1,8 @@
-namespace VpnSpeedAnalyzer
+namespace VpnSpeedAnalyzer.Models
 {
     public class IpInfo
     {
         public string Ip { get; set; } = "";
-        public string CountryCode { get; set; } = "";
         public string CountryName { get; set; } = "";
-        public string Asn { get; set; } = "";
-        public string Provider { get; set; } = "";
     }
 }
